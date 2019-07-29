efb5653256012312b9f1fa31185a433b987588b5
Eric Folks <efolks@live.com>
2019-07-29 11:34:42 -0400

    added styling to landing page, purple & yellow color scheme

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -7,5 +7,5 @@ import HomeScreen from '../screens/HomeScreen'
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: HomeScreen,
+  Main: HomeScreen
 }));
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,7 +15,7 @@ class HomeScreen extends React.Component {
   }
 
   componentDidMount() {
-    this.props.populateStartingActor()
+    this.props.populateStartingActorAndTargetActor()
   }
 
   handlePressSubmit(){
@@ -26,16 +26,15 @@ class HomeScreen extends React.Component {
 
   render() {
     return (
-    <Container>
+    <Container style={styles.container}>
       <Content>
           {this.props.isWinner ? <Text>You Won! Want to play again?</Text> : <Text />}
           {this.props.isLoser ? <Text>Oh no you lost. Want to play again?</Text> : <Text />}
           {this.props.isActive ? <Game style={styles.container} /> :
-          <View style={styles.container}>
-            <Text h2>Connect Your Way</Text>
-            <Text h2>To Kevin Bacon!</Text>
-            <Text h2>In Six Degrees!</Text>
-            <Button rounded onPress={this.handlePressSubmit} raised={true}><Text>Start Game</Text></Button>
+          <View style={styles.landingPageContainer}>
+            <Text h2 style={styles.landingPageTitle}>Welcome to Six Degrees</Text>
+            <Text style={styles.gameDescription}>The game where all that hardwork of watching summer movies on TBS finally pays off. Given a star, you must connect them to another star via their film credits in less than six degrees. Can you do it?</Text>
+            <Button rounded onPress={this.handlePressSubmit} raised={true}><Text>New Challenge</Text></Button>
           </View>
           }
       </Content>
@@ -49,7 +48,7 @@ class HomeScreen extends React.Component {
 const mapDispatch = (dispatch) => (
   {
     startGame: () => dispatch(toggleIsActiveGame()),
-    populateStartingActor: () => dispatch(fetchStartingAndTargetActor()),
+    populateStartingActorAndTargetActor: () => dispatch(fetchStartingAndTargetActor()),
     reset: () => dispatch(resetGame())
   }
 )
@@ -69,9 +68,32 @@ export default connect(mapState, mapDispatch)(HomeScreen)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5941A9',
     flexDirection: 'column',
-    alignItems: 'center'
+    // alignItems: 'center'
 
+  }, 
+  landingPageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '75%',
+    height: 800
+  },
+  landingPageTitle: {
+    paddingBottom: '10%',
+    fontSize: 75,
+    textAlign: 'center',
+    color: '#FFCA3A',
+    fontFamily: 'Marker Felt'
+  },
+  gameDescription: {
+    paddingBottom: '10%',
+    color: '#FFCA3A',
+    fontFamily: 'Marker Felt',
+    fontSize: 25,
+    textAlign: 'center'
   }
 });
